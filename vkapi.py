3bde0769b5d1099335a21675e5dd87df388d8419
idisin91 <idisin91@gmail.com>
2017-12-15 18:51:02 +0300

    token

--- vkapi.py
+++ vkapi.py
@@ -18,5 +18,6 @@ def send_message(user_id, token, message, attachment=""):
     api.messages.send(access_token=token, user_id=str(user_id), message=message, attachment=attachment)
 
 def post_poll(token, message):
-    api.wall.post(access_token=token, owner_id=112285736,message="poll")
+    pass
+    #api.wall.post(access_token=token, owner_id=112285736,message="poll")
 
