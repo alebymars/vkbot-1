ed408e884626c6940a672c48cd7381b3222521e8
idisin91 <idisin91@gmail.com>
2017-12-15 16:40:11 +0300

    encoding

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from flask import Flask, request, json
 from settings import *
 import messageHandler
--- settings.py
+++ settings.py
@@ -1,2 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 token = '95ec26ebf54f29da7f9e6508d78a96f065b9798da64e7ae5e0baa427d7c7ce7d245c951e0157e712510ad'
 confirmation_token = 'affb7bc9'
